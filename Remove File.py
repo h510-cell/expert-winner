1e48912eb089ee4a9eb29fa9b118b47087002978
h510-cell <58731327+h510-cell@users.noreply.github.com>
2020-09-11 13:22:34 +0530

    Add files via upload

---
+++ Remove File.py
@@ -0,0 +1,36 @@
+import os;
+import shutil;
+import time;
+
+var days="d"
+
+d=time.time()
+
+
+os.mkdir("Honey Bunny")
+path="Honey Bunny"
+os.path.exists(path)
+
+if(os.path.exists===True):
+   os.listdir()
+
+   os.walk(path)
+
+   os.join(path,"aagam hem")
+
+   #getting ctime of the file/folder
+   #time will be in seconds
+   ctime=os.stat(path).st_ctime
+
+   #returning the time
+   return ctime
+
+   if(ctime>d):
+   os.path.isfile("Honey Bunny") # Does Honey Bunny a file? Is it  file, or a folder?
+
+   if(path===file):
+       os.remove(path)
+   elif(path===file):
+       shutil.rtmtree()
+elif(os.path.exists===False):
+    print("Not Found")
